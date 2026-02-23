d07cd86a9c598e24cdfcb862104224be91c0d7a2
Arvind <arvindsingh.2021cs@technonjr>
2026-02-24 01:12:28 +0530

    Dockerized frontend and backend with nginx reverse proxy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+app.options("*", cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
